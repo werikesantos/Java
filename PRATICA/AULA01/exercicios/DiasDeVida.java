2b235122e8b38de731276d3fde4fc8b41beb2b20
werikesantos <werike.ms@gmail.com>
2021-03-04 00:22:18 -0300

    Ajustado

---
+++ PRATICA/AULA01/exercicios/DiasDeVida.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import javax.swing.JOptionPane;
+
+public class DiasDeVida {
+
+	public static void main(String[] args) {
+		/*
+		 * 6) Ler a idade de uma pessoa expressa em anos e exibir expressa em dias (considere que um ano tem 365 dias.
+		 * 
+		 */
+		
+		int ano = Integer.parseInt(JOptionPane.showInputDialog("Digite o ano de seu nascimento:"));
+		int idade = (2021 - ano);
+		int dias = (idade * 365);
+		JOptionPane.showMessageDialog(null, "Dias de vida: " +  dias);
+		
+	}
+
+}
---
+++ PRATICA/AULA01/exercicios/VidaAnoMesDia.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+import javax.swing.JOptionPane;
+
+public class VidaAnoMesDia {
+
+	public static void main(String[] args) {
+		/*
+		 * 7) Ler a idade de uma pessoa expressa em anos, meses e dias e exibir a idade dessa pessoa 
+		 * expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
+		 */
+		int ano = Integer.parseInt(JOptionPane.showInputDialog("Digite o ANO de seu nascimento:"));
+		int mes = Integer.parseInt(JOptionPane.showInputDialog("Digite o MÊS do seu nascimento:"));
+		int dia = Integer.parseInt(JOptionPane.showInputDialog("Digite o DIA do seu nascimento:"));
+		
+		int dias = dia + (ano * 365) + (mes * 30);
+		JOptionPane.showMessageDialog(null, "Idade expressa em dias: " +  dias);
+		
+	}
+
+}
