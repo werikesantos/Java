87dc3fe7dc3dcc01cbd8e684a1283352d2de06b0
werikesantos <werike.ms@gmail.com>
2021-03-06 17:45:13 -0300

    Finalizado exercicio 01 - Antecessor

---
+++ PRATICA/src/AULA01/exercicios/Antecessor.java
@@ -0,0 +1,18 @@
+package AULA01.exercicios;
+
+import javax.swing.JOptionPane;
+
+public class Antecessor {
+
+    public static void main(String[] args) {
+    /* 
+     * 4) Ler um valor inteiro e exibir seu antecessor.
+     * 
+     */
+    int valor = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor:"));
+
+    int antecessor = (valor - 1); 
+
+    JOptionPane.showMessageDialog(null, "Antecessor " + antecessor);
+    }
+}
